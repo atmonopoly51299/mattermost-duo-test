e979b96f028eb6e0f140e2aa80d0e434c8ce6542
Lê Phạm Ngọc Yến <lephamngocyen@gmail.com>
2019-12-20 19:29:49 +0700

    update new folders

---
+++ Scripts/5_Team_Settings/Edit_Team_Name/Script1576844830662.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.youtube.com/watch?v=BVomQtrtMTM&list=RDm_qlgFQs7E4&index=3')
+
+WebUI.click(findTestObject('5_Team_Settings/5.6_Edit_Team_Name/Page_Town Square - TRIII Mattermost/svg'))
+
+WebUI.click(findTestObject('5_Team_Settings/5.6_Edit_Team_Name/Page_Town Square - TRIII Mattermost/button_Team Settings'))
+
+WebUI.click(findTestObject('5_Team_Settings/5.6_Edit_Team_Name/Page_Town Square - TRIII Mattermost/button_General'))
+
+WebUI.click(findTestObject('5_Team_Settings/5.6_Edit_Team_Name/Page_Town Square - TRIII Mattermost/span_Edit'))
+
+WebUI.doubleClick(findTestObject('5_Team_Settings/5.6_Edit_Team_Name/Page_Town Square - TRIII Mattermost/input_Team Name_teamName'))
+
+WebUI.setText(findTestObject('5_Team_Settings/5.6_Edit_Team_Name/Page_Town Square - TRIII Mattermost/input_Team Name_teamName'), 
+    'New Team Name')
+
+WebUI.click(findTestObject('5_Team_Settings/5.6_Edit_Team_Name/Page_Town Square - TRIII Mattermost/span_Save'))
+
+not_run: WebUI.closeBrowser()
+
---
+++ Scripts/6_Account_Settings/Edit_Full_Name/Script1576844780583.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://test-app-1311.herokuapp.com/triii/channels/town-square')
+
+WebUI.click(findTestObject('6_Account_Settings/Edit_Full_Name/Page_Town Square - New_Team_Name Mattermost/svg'))
+
+WebUI.click(findTestObject('6_Account_Settings/Edit_Full_Name/Page_Town Square - New_Team_Name Mattermost/button_Account Settings'))
+
+WebUI.click(findTestObject('6_Account_Settings/Edit_Full_Name/Page_Town Square - New_Team_Name Mattermost/span_Edit'))
+
+WebUI.setText(findTestObject('6_Account_Settings/Edit_Full_Name/Page_Town Square - New_Team_Name Mattermost/input_First Name_firstName'), 
+    'First Name')
+
+WebUI.setText(findTestObject('6_Account_Settings/Edit_Full_Name/Page_Town Square - New_Team_Name Mattermost/input_Last Name_lastName'), 
+    'Last Name')
+
+WebUI.click(findTestObject('6_Account_Settings/Edit_Full_Name/Page_Town Square - New_Team_Name Mattermost/span_Save'))
+
+WebUI.closeBrowser()
+
