ca53cd619366b131a4f356965b174b43abb62d98
Lê Phạm Ngọc Yến <lephamngocyen@gmail.com>
2019-12-21 20:20:24 +0700

    dont do this again

---
+++ Test Suites/Suite_2.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite_2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2dbb733a-e2be-406b-a3a3-65ad126f8059</testSuiteGuid>
+</TestSuiteEntity>
